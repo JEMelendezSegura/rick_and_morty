30c427ed9e73a813f4c16cd4e281795a413f35b2
unknown <esaums@gmail.com>
2023-06-05 22:32:16 -0600

    Arranque proyecto React-Intro

---
+++ src/components/Cards.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Card from "./Card";
+
+export default function Cards(props) {
+   return (
+   <div>
+         <ul>
+         {props.characters.map((pj) => (
+            <Card
+            key={pj.id}
+            id={pj.id}
+            name={pj.name}
+            status={pj.status}
+            species={pj.species}
+            gender={pj.gender}
+            origin={pj.origin.name}
+            image={pj.image}
+            onClose={() => window.alert('Emulamos que se cierra la card')}/>
+         ))}
+         </ul>
+   </div>
+   );
+}
+
+
