92c3863d623560095aa10b80763879377cb070e9
unknown <esaums@gmail.com>
2023-08-12 02:33:40 -0600

    PI completo

--- Server/src/controllers/login.js
+++ Server/src/controllers/login.js
@@ -1,15 +1,24 @@
-const users = require('../utils/users');
+const {User} = require("../DB_connection");
 
-module.exports = (req, res) => {
-  const { email, password } = req.query;
+const login = async (req, res) =>{
+    const {email, password} = req.query;
+    try {
+        if (!email || !password){
+            return res.status(400).json({ error: 'Faltan datos' });
+        }
+        const loggedUser = await User.findOne({
+            where: { email },
+        });
+        if (!loggedUser)
+        return res.status(404).json({ error: 'Usuario no encontrado' });
+    
+        return loggedUser.password === password
+        ? res.status(200).json({ access: true })
+        : res.status(401).json({ error: 'Contraseña incorrecta' });
 
-  const user = users.find(
-    (user) => user.email === email && user.password === password
-  );
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
 
-  if (user) {
-    return res.status(200).json({ access: true });
-  } else {
-    return res.status(403).json({ access: false });
-  }
-};
+module.exports = login;
--- Server/src/utils/users.js
+++
@@ -1,1 +0,0 @@
-module.exports = [{ email: 'esaums@gmail.com', password: 'netflix1' }];
